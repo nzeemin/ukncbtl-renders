0617289b925ec10e26ae39a62347b69fa7bb8edd
Nikita Zimin <nzeemin@gmail.com>
2015-09-22 18:08:25 +0300

    AStyle batch script and options.

---
+++ !astyle.bat
@@ -0,0 +1,7 @@
+@echo off
+set ASTYLEEXE=c:\bin\astyle.exe
+set ASTYLEOPT=-n --options=astyle-cpp-options
+%ASTYLEEXE% %ASTYLEOPT% *.h *.cpp --exclude=Resource.h --exclude=stdafx.h --exclude=stdafx.cpp
+%ASTYLEEXE% %ASTYLEOPT% RenderVfw\*.cpp --exclude=stdafx.h --exclude=stdafx.cpp
+%ASTYLEEXE% %ASTYLEOPT% RenderDX9\*.cpp --exclude=stdafx.h --exclude=stdafx.cpp
+%ASTYLEEXE% %ASTYLEOPT% RenderOpenGL\*.cpp --exclude=stdafx.h --exclude=stdafx.cpp
